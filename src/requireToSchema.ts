07e640ba08f6d03e1c21d2a82833a4b4f86f106c
Konstantin Kolomeitsev <konstantin.kolomeitsev@ps.kz>
2019-11-21 19:15:36 +0600

    feat: add requireToSchema

--- src/requireToSchema.ts
+++ src/requireToSchema.ts
@@ -3,6 +3,8 @@ import {
   ObjectTypeComposer,
   ObjectTypeComposerFieldConfigMapDefinition,
   upperFirst,
+  forEachKey,
+  schemaComposer,
 } from 'graphql-compose';
 import {
   RequireAstResult,
@@ -24,25 +26,64 @@ export function requireToSchema<TContext = any>(ast: RequireAstResult): SchemaCo
 function populateRoot(
   sc: SchemaComposer<any>,
   rootName: 'Query' | 'Mutation' | 'Subscription',
-  ast: RequireAstRootTypeNode
+  astRootNode: RequireAstRootTypeNode
 ) {
   const tc = sc[rootName];
-  const fields = createFields(ast, rootName);
-  tc.addNestedFields(fields);
+  Object.keys(astRootNode.children).forEach((key) => {
+    createFields(astRootNode.children[key], rootName, tc);
+  });
 }
 
 function createFields(
-  ast: RequireAstDirNode | RequireAstRootTypeNode,
-  suffix: string
-): ObjectTypeComposerFieldConfigMapDefinition<any, any> {
-  const fields = {};
+  ast: RequireAstDirNode | RequireAstFileNode,
+  suffix: string,
+  parent: ObjectTypeComposer
+) {
+  if (ast.kind === 'file') {
+    if (ast.name !== 'index') {
+      if (!ast.code.default) throw new Error('File must return field config by default export');
 
-  Object.keys(ast.children).forEach((fieldName) => {
-    const node = ast.children[fieldName];
-    if (node.kind === 'dir') {
+      parent.addNestedFields({
+        [ast.name]: ast.code.default,
+      });
     }
-    fields[fieldName] = '';
-  });
+  } else if (ast.kind === 'dir') {
+    let name = ast.name;
+    let typeDefs: any = {};
 
-  return fields;
+    if (name.indexOf('.') !== -1) {
+      const namesArray = name.split('.');
+
+      if (namesArray.length === 1) {
+        name = namesArray[0];
+      } else {
+        name = namesArray.reduce((finalName, current) => {
+          return finalName + upperFirst(current);
+        }, '');
+      }
+    }
+
+    if (ast.children['index'] && ast.children['index'].kind === 'file') {
+      typeDefs = ast.children['index'].code.default || {};
+    }
+
+    if (typeDefs.type) {
+      if (typeof typeDefs.type === 'string') {
+        typeDefs.type = schemaComposer.createObjectTC(typeDefs.type);
+      }
+    } else {
+      typeDefs.type = schemaComposer.createObjectTC(`${suffix}${upperFirst(name)}`);
+    }
+
+    Object.keys(ast.children).forEach((key) => {
+      createFields(ast.children[key], name, typeDefs.type);
+    });
+
+    parent.addNestedFields({
+      [ast.name]: {
+        resolve: () => ({}),
+        ...typeDefs,
+      },
+    });
+  }
 }
