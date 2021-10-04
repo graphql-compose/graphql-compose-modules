99da8c2b82bb4dfec24bc47297189ca4af346720
nodkz <pavel.chertorogov@gmail.com>
2021-10-04 17:57:45 +0600

    chore: update dependencies

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,7 +1,10 @@
 import { ObjectTypeComposerFieldConfigAsObjectDefinition } from 'graphql-compose';
 
-export type FieldConfig<TContext = any, TArgs = any, TSource = any> =
-  ObjectTypeComposerFieldConfigAsObjectDefinition<TSource, TContext, TArgs>;
+export type FieldConfig<
+  TContext = any,
+  TArgs = any,
+  TSource = any
+> = ObjectTypeComposerFieldConfigAsObjectDefinition<TSource, TContext, TArgs>;
 
 export type NamespaceConfig<TContext = any, TArgs = any, TSource = any> = Partial<
   ObjectTypeComposerFieldConfigAsObjectDefinition<TSource, TContext, TArgs>
